f95cef32d6cebfecd31be2909a2660d61e95b571
Luiza <luizamf89@gmail.com>
2014-10-22 20:13:12 -0200

    Subindo teste

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,6 +5,26 @@
     // ==================================================
     // General
     // ==================================================
+	$("header").addClass("fechada");
+	     
+	$(".menuIcon").click(function() {
+		if($("header").hasClass("fechada")) {
+			$("header").animate({'height':'180px'},{queue:false, duration:300}).removeClass("fechada");
+		}
+		else {
+			$("header").animate({'height':'65px'},{queue:false, duration:300}).addClass("fechada");
+		}
+	});
 
+	$(window).resize(function() {
+	var tamanhoViewport = $(window).width();
+	if (tamanhoViewport > 768) {
+	$("header").css("height","340px").addClass("fechada");
+	} else {
+	$("header").css("height","65px").addClass("fechada");
+	}
+	});	
 
 })(jQuery);
+
+
